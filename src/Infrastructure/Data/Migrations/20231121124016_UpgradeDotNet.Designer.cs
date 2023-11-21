3465e709afdeb51ed4a542eb4c040e0b5198a19b
Hjalte Parner <hjalte@ices.dk>
2023-11-21 13:50:38 +0100

    Upgrade to DotNet 8

--- src/Infrastructure/Data/Migrations/20231121124016_UpgradeDotNet.Designer.cs
+++ src/Infrastructure/Data/Migrations/20231121124016_UpgradeDotNet.Designer.cs
@@ -4,6 +4,7 @@ using CA_Angular.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CA_Angular.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231121124016_UpgradeDotNet")]
+    partial class UpgradeDotNet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
